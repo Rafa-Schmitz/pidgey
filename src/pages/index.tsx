eae6f05772755746e4ca2204faf27489d3d8b56b
rafa-schmitz <rafaelsschmitz98@gmail.com>
2023-07-23 21:59:05 -0300

    Adds a bit of styling, Next image component and mapping all of users recommendations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,66 @@
-import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { SignInButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
+
 import { api } from "~/utils/api";
+import type { RouterOutputs } from "~/utils/api";
+
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import Image from "next/image";
+
+dayjs.extend(relativeTime);
+
+const CreatePost = () => {
+  const { user } = useUser();
+
+  if (!user) return null;
+
+  return (
+    <div className="flex gap-2 w-full">
+      <Image
+        src={user.profileImageUrl}
+        alt="Profile Image"
+        className="w-14 h-14 rounded-full"
+        width={56}
+        height={56}
+      />
+      <input type="text" placeholder="Create your message!" className="bg-transparent indent-4 grow outline-none" />
+    </div>
+  )
+}
+
+type PostWithUser = RouterOutputs["recommendations"]["getAll"][number];
+
+const RecommendationView = (props: PostWithUser) => {
+  const { recommendation, author } = props;
+
+  return (
+    <div key={recommendation.id} className="flex gap-3 p-4 border-b border-slate-400">
+      <Image
+        src={author.profileImageUrl}
+        alt={`${author.username}'s profile image`}
+        className="w-14 h-14 rounded-full"
+        width={56}
+        height={56}
+      />
+      <div className="flex flex-col">
+        <div className="flex gap-2 text-slate-300">
+          <span>{`@${author.username}`}</span>
+          <span className="font-thin">{dayjs(recommendation.createdAt).fromNow()}</span>
+        </div>
+        <span className="">{recommendation.content}</span>
+      </div>
+    </div>
+  )
+}
 
 export default function Home() {
   const user = useUser();
-  const { data } = api.recommendations.getAll.useQuery();
+  const { data, isLoading } = api.recommendations.getAll.useQuery();
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (!data) return <div>No posts to show...</div>;
 
   return (
     <>
@@ -14,15 +70,17 @@ export default function Home() {
         <meta name="keywords" content="Software Developer, Front-End, Back-End, Full-Stack, Typescript, NextJS, React, Java, Spring, SQL, Mobile Development" />
         <link rel="icon" href="/main-logo.png" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div>
-          {user?.isSignedIn && <SignOutButton />}
-          {!user?.isSignedIn && <SignInButton />}
-        </div>
-        <div>
-          {data?.map((recommendations) => (
-            <div key={recommendations?.id}>{recommendations?.content}</div>
-          ))}
+      <main className="flex h-screen justify-center">
+        <div className="w-full h-full md:max-w-2xl border-x border-slate-400">
+          <div className="border-b border-slate-400 p-4 flex">
+            {user?.isSignedIn && <CreatePost />}
+            {!user?.isSignedIn && <div className="flex justify-center"><SignInButton /></div>}
+          </div>
+          <div className="flex flex-col">
+            {data?.map((post) => (
+              <RecommendationView {...post} key={post.recommendation.id} />
+            ))}
+          </div>
         </div>
       </main>
     </>
--- src/server/api/routers/recommendations.ts
+++ src/server/api/routers/recommendations.ts
@@ -1,8 +1,48 @@
+import { clerkClient } from "@clerk/nextjs";
+import type { User } from "@clerk/nextjs/dist/types/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl,
+  };
+};
+
 export const recommendationsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.recommendation.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const recommendations = await ctx.prisma.recommendation.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: recommendations.map(
+          (recommendation) => recommendation.authorId
+        ),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    return recommendations.map((recommendation) => {
+      const author = users.find((user) => user.id === recommendation.authorId);
+
+      if (!author?.username)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for post not found",
+        });
+
+      return {
+        recommendation,
+        author: {
+          ...author,
+          username: author.username,
+        },
+      };
+    });
   }),
 });
