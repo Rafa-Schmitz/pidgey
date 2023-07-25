228a2a416d7f1aa3405b39da265a5d0d89ef54e5
rafa-schmitz <rafaelsschmitz98@gmail.com>
2023-07-24 21:02:53 -0300

    Ignoring build errors on vercel cause we're it uses github CI now, also implements swc minifier

--- next.config.mjs
+++ next.config.mjs
@@ -26,6 +26,13 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  swcMinify: true
 };
 
 export default config;
